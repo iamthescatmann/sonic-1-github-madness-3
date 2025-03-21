7389b47da107a28fc4f4e1f2e7cc4bba3d8fd797
Anakama <109427414+AnakamaTH@users.noreply.github.com>
2025-03-21 21:14:55 +0300

    Warning message in build.bat cus why not?!1

--- build.bat
+++ build.bat
@@ -1,4 +1,10 @@
 @echo off
+echo Please restore the old code before building, you might break the disassembly
+echo Press anything to continue
+pause>nul
+cls
+echo Building...
+
 asm68k /o op+ /o os+ /o ow+ /o oz+ /o oaq+ /o osq+ /o omq+ /p /o ae- sonic1.asm, s1built.bin, s1built.sym, sonic1.lst
 convsym sonic1.lst s1built.bin -input asm68k_lst -inopt "/localSign=@ /localJoin=. /ignoreMacroDefs+ /ignoreMacroExp- /addMacrosAsOpcodes+" -a
 fixheadr.exe s1built.bin
