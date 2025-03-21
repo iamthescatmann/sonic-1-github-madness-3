11c59d3f0fcaac2c69cacead5c9cc3c551128bcf
Dingish <badfnfsongs@gmail.com>
2025-03-21 13:47:49 -0500

    bug

--- _maps/obj40.asm
+++ _maps/obj40.asm
@@ -1,32 +1,32 @@
-Map_d6ce:
-Map_d6ce_0: 	dc.w Map_d6ce_E-Map_d6ce
-Map_d6ce_2: 	dc.w Map_d6ce_23-Map_d6ce
-Map_d6ce_4: 	dc.w Map_d6ce_38-Map_d6ce
-Map_d6ce_6: 	dc.w Map_d6ce_52-Map_d6ce
-Map_d6ce_8: 	dc.w Map_d6ce_58-Map_d6ce
-Map_d6ce_A: 	dc.w Map_d6ce_5E-Map_d6ce
-Map_d6ce_C: 	dc.w Map_d6ce_64-Map_d6ce
-Map_d6ce_E: 	dc.b $4
+Map_cedc:
+Map_cedc_0: 	dc.w Map_cedc_E-Map_cedc
+Map_cedc_2: 	dc.w Map_cedc_23-Map_cedc
+Map_cedc_4: 	dc.w Map_cedc_38-Map_cedc
+Map_cedc_6: 	dc.w Map_cedc_52-Map_cedc
+Map_cedc_8: 	dc.w Map_cedc_58-Map_cedc
+Map_cedc_A: 	dc.w Map_cedc_5E-Map_cedc
+Map_cedc_C: 	dc.w Map_cedc_64-Map_cedc
+Map_cedc_E: 	dc.b $4
 	dc.b $F0, $D, $0, $0, $EC
 	dc.b $0, $C, $0, $8, $EC
 	dc.b $F8, $1, $0, $C, $C
 	dc.b $8, $8, $0, $E, $F4
-Map_d6ce_23: 	dc.b $4
+Map_cedc_23: 	dc.b $4
 	dc.b $F1, $D, $0, $0, $EC
 	dc.b $1, $C, $0, $8, $EC
 	dc.b $F9, $1, $0, $C, $C
 	dc.b $9, $8, $0, $11, $F4
-Map_d6ce_38: 	dc.b $5
+Map_cedc_38: 	dc.b $5
 	dc.b $F0, $D, $0, $0, $EC
 	dc.b $0, $C, $0, $14, $EC
 	dc.b $F8, $1, $0, $C, $C
 	dc.b $8, $4, $0, $18, $EC
 	dc.b $8, $4, $0, $12, $FC
-Map_d6ce_52: 	dc.b $1
+Map_cedc_52: 	dc.b $1
 	dc.b $FA, $0, $0, $1A, $10
-Map_d6ce_58: 	dc.b $1
+Map_cedc_58: 	dc.b $1
 	dc.b $FA, $0, $0, $1B, $10
-Map_d6ce_5E: 	dc.b $1
+Map_cedc_5E: 	dc.b $1
 	dc.b $FA, $0, $0, $1C, $10
-Map_d6ce_64: 	dc.b $0
+Map_cedc_64: 	dc.b $0
 	even
