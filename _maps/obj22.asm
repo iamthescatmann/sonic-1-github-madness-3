807fa8f183ef093069070c52c7dcda6682f2eab4
Dingish <badfnfsongs@gmail.com>
2025-03-21 13:06:26 -0500

    buzz bomber

--- _maps/obj22.asm
+++ _maps/obj22.asm
@@ -1,51 +1,50 @@
-; ---------------------------------------------------------------------------
-; Sprite mappings - Buzz Bomber	enemy
-; ---------------------------------------------------------------------------
-		dc.w byte_9A42-Map_obj22, byte_9A61-Map_obj22
-		dc.w byte_9A80-Map_obj22, byte_9AA4-Map_obj22
-		dc.w byte_9AC8-Map_obj22, byte_9AE7-Map_obj22
-byte_9A42:	dc.b 6
-		dc.b $F4, 9, 0,	0, $E8
-		dc.b $F4, 9, 0,	$F, 0
-		dc.b 4,	8, 0, $15, $E8
-		dc.b 4,	4, 0, $18, 0
-		dc.b $F1, 8, 0,	$1A, $EC
-		dc.b $F1, 4, 0,	$1D, 4
-byte_9A61:	dc.b 6
-		dc.b $F4, 9, 0,	0, $E8
-		dc.b $F4, 9, 0,	$F, 0
-		dc.b 4,	8, 0, $15, $E8
-		dc.b 4,	4, 0, $18, 0
-		dc.b $F4, 8, 0,	$1F, $EC
-		dc.b $F4, 4, 0,	$22, 4
-byte_9A80:	dc.b 7
-		dc.b 4,	0, 0, $30, $C
-		dc.b $F4, 9, 0,	0, $E8
-		dc.b $F4, 9, 0,	$F, 0
-		dc.b 4,	8, 0, $15, $E8
-		dc.b 4,	4, 0, $18, 0
-		dc.b $F1, 8, 0,	$1A, $EC
-		dc.b $F1, 4, 0,	$1D, 4
-byte_9AA4:	dc.b 7
-		dc.b 4,	4, 0, $31, $C
-		dc.b $F4, 9, 0,	0, $E8
-		dc.b $F4, 9, 0,	$F, 0
-		dc.b 4,	8, 0, $15, $E8
-		dc.b 4,	4, 0, $18, 0
-		dc.b $F4, 8, 0,	$1F, $EC
-		dc.b $F4, 4, 0,	$22, 4
-byte_9AC8:	dc.b 6
-		dc.b $F4, $D, 0, 0, $EC
-		dc.b 4,	$C, 0, 8, $EC
-		dc.b 4,	0, 0, $C, $C
-		dc.b $C, 4, 0, $D, $F4
-		dc.b $F1, 8, 0,	$1A, $EC
-		dc.b $F1, 4, 0,	$1D, 4
-byte_9AE7:	dc.b 4
-		dc.b $F4, $D, 0, 0, $EC
-		dc.b 4,	$C, 0, 8, $EC
-		dc.b 4,	0, 0, $C, $C
-		dc.b $C, 4, 0, $D, $F4
-		dc.b $F4, 8, 0,	$1F, $EC
-		dc.b $F4, 4, 0,	$22, 4
-		even
+Map_e97a:
+Map_e97a_0: 	dc.w Map_e97a_C-Map_e97a
+Map_e97a_2: 	dc.w Map_e97a_2B-Map_e97a
+Map_e97a_4: 	dc.w Map_e97a_4A-Map_e97a
+Map_e97a_6: 	dc.w Map_e97a_6E-Map_e97a
+Map_e97a_8: 	dc.w Map_e97a_92-Map_e97a
+Map_e97a_A: 	dc.w Map_e97a_B1-Map_e97a
+Map_e97a_C: 	dc.b $6
+	dc.b $F4, $9, $0, $0, $E8
+	dc.b $F4, $9, $0, $F, $0
+	dc.b $4, $8, $0, $15, $E8
+	dc.b $4, $4, $0, $18, $0
+	dc.b $F1, $8, $0, $1A, $EC
+	dc.b $F1, $4, $0, $1D, $4
+Map_e97a_2B: 	dc.b $6
+	dc.b $F4, $9, $0, $0, $E8
+	dc.b $F4, $9, $0, $F, $0
+	dc.b $4, $8, $0, $15, $E8
+	dc.b $4, $4, $0, $18, $0
+	dc.b $F4, $8, $0, $1F, $EC
+	dc.b $F4, $4, $0, $22, $4
+Map_e97a_4A: 	dc.b $7
+	dc.b $4, $0, $0, $30, $C
+	dc.b $F4, $9, $0, $0, $E8
+	dc.b $F4, $9, $0, $F, $0
+	dc.b $4, $8, $0, $15, $E8
+	dc.b $4, $4, $0, $18, $0
+	dc.b $F1, $8, $0, $1A, $EC
+	dc.b $F1, $4, $0, $1D, $4
+Map_e97a_6E: 	dc.b $7
+	dc.b $4, $4, $0, $31, $C
+	dc.b $F4, $9, $0, $0, $E8
+	dc.b $F4, $9, $0, $F, $0
+	dc.b $4, $8, $0, $15, $E8
+	dc.b $4, $4, $0, $18, $0
+	dc.b $F4, $8, $0, $1F, $EC
+	dc.b $F4, $4, $0, $22, $4
+Map_e97a_92: 	dc.b $6
+	dc.b $F4, $D, $0, $0, $EC
+	dc.b $4, $C, $0, $8, $EC
+	dc.b $4, $0, $0, $C, $C
+	dc.b $C, $4, $0, $D, $F4
+	dc.b $F1, $8, $0, $1A, $EC
+	dc.b $F1, $4, $0, $1D, $4
+Map_e97a_B1: 	dc.b $4
+	dc.b $F4, $D, $0, $0, $EC
+	dc.b $4, $C, $0, $8, $EC
+	dc.b $4, $0, $0, $C, $C
+	dc.b $C, $4, $0, $D, $F4
+	even
