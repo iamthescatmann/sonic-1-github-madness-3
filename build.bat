b8bce549f3527d2c5ff52d8ff60348cc09c5fdba
Min3rR <88860568+Min3rREALS@users.noreply.github.com>
2025-03-21 14:22:32 -0400

    Update build.bat

--- build.bat
+++ build.bat
@@ -3,7 +3,7 @@ echo Please restore the old code before building (especially the asm files) or y
 echo Press anything to continue
 pause>nul
 cls
-echo Building...
+echo POOPING...PICKING BOOGERS...IM JUST SAYING THAT IT CAN GET REALLY NASTY ON YOUR SEAT.DONT STREAK IT
 
 asm68k /o op+ /o os+ /o ow+ /o oz+ /o oaq+ /o osq+ /o omq+ /p /o ae- sonic1.asm, s1built.bin, s1built.sym, sonic1.lst
 convsym sonic1.lst s1built.bin -input asm68k_lst -inopt "/localSign=@ /localJoin=. /ignoreMacroDefs+ /ignoreMacroExp- /addMacrosAsOpcodes+" -a
