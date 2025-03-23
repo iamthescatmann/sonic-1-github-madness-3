849fe3038fd9615afcc1af65e8978495b8e512f4
conic the cat <yazn0996@gmail.com>
2025-03-23 16:15:08 +0300

    THE CRAIG
    and makoto kino from sailor moon
    so kino
    pun not intended
    and sbz2 music

--- _anim/obj2D.asm
+++ _anim/obj2D.asm
@@ -1,12 +1,12 @@
 ; ---------------------------------------------------------------------------
-; Animation script - Burrobot enemy
+; Animation script - Burrobot enemy but craig
 ; ---------------------------------------------------------------------------
 		dc.w byte_AE4C-Ani_obj2D
 		dc.w byte_AE50-Ani_obj2D
 		dc.w byte_AE54-Ani_obj2D
 		dc.w byte_AE58-Ani_obj2D
-byte_AE4C:	dc.b 3,	0, 6, $FF
-byte_AE50:	dc.b 3,	0, 1, $FF
-byte_AE54:	dc.b 3,	2, 3, $FF
+byte_AE4C:	dc.b 3,	0, $FF ; wait
+byte_AE50:	dc.b 3,	0, 1, $FF ; waddle
+byte_AE54:	dc.b 1,	0, 1, 2, 3, 4, 5, 6, 7, 8, 3, 9, $A, $FF ; speen
 byte_AE58:	dc.b 3,	4, $FF
 		even
--- _maps/obj6E.asm
+++ _maps/obj6E.asm
@@ -1,37 +1,41 @@
-; ---------------------------------------------------------------------------
-; Sprite mappings - electrocution orbs (SBZ)
-; ---------------------------------------------------------------------------
-		dc.w byte_161EC-Map_obj6E, byte_161F7-Map_obj6E
-		dc.w byte_16207-Map_obj6E, byte_16221-Map_obj6E
-		dc.w byte_16236-Map_obj6E, byte_16255-Map_obj6E
-byte_161EC:	dc.b 2
-		dc.b $F8, 4, $60, 0, $F8
-		dc.b 0,	6, $40,	2, $F8
-byte_161F7:	dc.b 3
-		dc.b $F8, 5, 0,	8, $F8
-		dc.b $F8, 4, $60, 0, $F8
-		dc.b 0,	6, $40,	2, $F8
-byte_16207:	dc.b 5
-		dc.b $F8, 5, 0,	8, $F8
-		dc.b $F8, 4, $60, 0, $F8
-		dc.b 0,	6, $40,	2, $F8
-		dc.b $F6, $D, 0, $C, 8
-		dc.b $F6, $D, 8, $C, $DC
-byte_16221:	dc.b 4
-		dc.b $F8, 4, $60, 0, $F8
-		dc.b 0,	6, $40,	2, $F8
-		dc.b $F6, $D, 0, $C, 8
-		dc.b $F6, $D, 8, $C, $DC
-byte_16236:	dc.b 6
-		dc.b $F8, 4, $60, 0, $F8
-		dc.b 0,	6, $40,	2, $F8
-		dc.b $F6, $D, $10, $C, 8
-		dc.b $F6, $D, $18, $C, $DC
-		dc.b $F6, $D, 0, $C, $24
-		dc.b $F6, $D, 8, $C, $C0
-byte_16255:	dc.b 4
-		dc.b $F8, 4, $60, 0, $F8
-		dc.b 0,	6, $40,	2, $F8
-		dc.b $F6, $D, $10, $C, $24
-		dc.b $F6, $D, $18, $C, $C0
-		even
+Map_26ed:
+Map_26ed_0: 	dc.w Map_26ed_C-Map_26ed
+Map_26ed_2: 	dc.w Map_26ed_12-Map_26ed
+Map_26ed_4: 	dc.w Map_26ed_1D-Map_26ed
+Map_26ed_6: 	dc.w Map_26ed_3C-Map_26ed
+Map_26ed_8: 	dc.w Map_26ed_65-Map_26ed
+Map_26ed_A: 	dc.w Map_26ed_84-Map_26ed
+Map_26ed_C: 	dc.b $1
+	dc.b $8, $5, $0, $0, $F8
+Map_26ed_12: 	dc.b $2
+	dc.b $F8, $A, $0, $4, $F4
+	dc.b $10, $8, $0, $D, $F4
+Map_26ed_1D: 	dc.b $6
+	dc.b $F8, $A, $0, $4, $F4
+	dc.b $10, $8, $0, $D, $F4
+	dc.b $F6, $5, $0, $10, $C
+	dc.b $F6, $5, $0, $10, $1C
+	dc.b $F6, $5, $8, $10, $D4
+	dc.b $F6, $5, $8, $10, $E4
+Map_26ed_3C: 	dc.b $8
+	dc.b $F8, $A, $0, $4, $F4
+	dc.b $10, $8, $0, $D, $F4
+	dc.b $F6, $5, $0, $10, $C
+	dc.b $F6, $5, $0, $10, $1C
+	dc.b $F6, $5, $8, $10, $E4
+	dc.b $F6, $5, $0, $10, $2C
+	dc.b $F6, $5, $8, $10, $D4
+	dc.b $F6, $5, $8, $10, $C4
+Map_26ed_65: 	dc.b $6
+	dc.b $F8, $A, $0, $4, $F4
+	dc.b $10, $8, $0, $D, $F4
+	dc.b $F6, $5, $0, $10, $1C
+	dc.b $F6, $5, $0, $10, $2C
+	dc.b $F6, $5, $8, $10, $D4
+	dc.b $F6, $5, $8, $10, $C4
+Map_26ed_84: 	dc.b $4
+	dc.b $F8, $A, $0, $4, $F4
+	dc.b $10, $8, $0, $D, $F4
+	dc.b $F6, $5, $0, $10, $2C
+	dc.b $F6, $5, $8, $10, $C4
+	even
