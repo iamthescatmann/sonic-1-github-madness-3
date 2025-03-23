05aa121605db597d205012aaf23b31e7a229df2f
dawid from da 32bit <tetakkrme6@gmail.com>
2025-03-23 10:23:41 +0100

    bill newton crabmeat and somethin else idk

--- _maps/obj1F.asm
+++ _maps/obj1F.asm
@@ -1,39 +1,33 @@
-; ---------------------------------------------------------------------------
-; Sprite mappings - Crabmeat enemy (GHZ, SYZ)
-; ---------------------------------------------------------------------------
-		dc.w byte_975E-Map_obj1F, byte_9773-Map_obj1F
-		dc.w byte_9788-Map_obj1F, byte_979D-Map_obj1F
-		dc.w byte_97B2-Map_obj1F, byte_97D1-Map_obj1F
-		dc.w byte_97D7-Map_obj1F
-byte_975E:	dc.b 4
-		dc.b $F0, 9, 0,	0, $E8
-		dc.b $F0, 9, 8,	0, 0
-		dc.b 0,	5, 0, 6, $F0
-		dc.b 0,	5, 8, 6, 0
-byte_9773:	dc.b 4
-		dc.b $F0, 9, 0,	$A, $E8
-		dc.b $F0, 9, 0,	$10, 0
-		dc.b 0,	5, 0, $16, $F0
-		dc.b 0,	9, 0, $1A, 0
-byte_9788:	dc.b 4
-		dc.b $EC, 9, 0,	0, $E8
-		dc.b $EC, 9, 8,	0, 0
-		dc.b $FC, 5, 8,	6, 0
-		dc.b $FC, 6, 0,	$20, $F0
-byte_979D:	dc.b 4
-		dc.b $EC, 9, 0,	$A, $E8
-		dc.b $EC, 9, 0,	$10, 0
-		dc.b $FC, 9, 0,	$26, 0
-		dc.b $FC, 6, 0,	$2C, $F0
-byte_97B2:	dc.b 6
-		dc.b $F0, 4, 0,	$32, $F0
-		dc.b $F0, 4, 8,	$32, 0
-		dc.b $F8, 9, 0,	$34, $E8
-		dc.b $F8, 9, 8,	$34, 0
-		dc.b 8,	4, 0, $3A, $F0
-		dc.b 8,	4, 8, $3A, 0
-byte_97D1:	dc.b 1
-		dc.b $F8, 5, 0,	$3C, $F8
-byte_97D7:	dc.b 1
-		dc.b $F8, 5, 0,	$40, $F8
-		even
+Map_1514:
+Map_1514_0: 	dc.w Map_1514_E-Map_1514
+Map_1514_2: 	dc.w Map_1514_1E-Map_1514
+Map_1514_4: 	dc.w Map_1514_2E-Map_1514
+Map_1514_6: 	dc.w Map_1514_3E-Map_1514
+Map_1514_8: 	dc.w Map_1514_4E-Map_1514
+Map_1514_A: 	dc.w Map_1514_5E-Map_1514
+Map_1514_C: 	dc.w Map_1514_64-Map_1514
+Map_1514_E: 	dc.b $3
+	dc.b $F8, $E, $0, $0, $E4
+	dc.b $F0, $4, $0, $C, $FC
+	dc.b $F8, $A, $0, $E, $4
+Map_1514_1E: 	dc.b $3
+	dc.b $F8, $E, $0, $0, $E4
+	dc.b $F0, $4, $0, $C, $FC
+	dc.b $F8, $A, $0, $E, $4
+Map_1514_2E: 	dc.b $3
+	dc.b $F8, $E, $0, $0, $E4
+	dc.b $F0, $4, $0, $C, $FC
+	dc.b $F8, $A, $0, $E, $4
+Map_1514_3E: 	dc.b $3
+	dc.b $F8, $E, $0, $0, $E4
+	dc.b $F0, $4, $0, $C, $FC
+	dc.b $F8, $A, $0, $E, $4
+Map_1514_4E: 	dc.b $3
+	dc.b $F8, $E, $0, $0, $E4
+	dc.b $F0, $4, $0, $C, $FC
+	dc.b $F8, $A, $0, $E, $4
+Map_1514_5E: 	dc.b $1
+	dc.b $F8, $5, $0, $17, $F8
+Map_1514_64: 	dc.b $1
+	dc.b $F8, $5, $20, $17, $F8
+	even
